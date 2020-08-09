bb5c03afe4f2319be5f2db38b59dc719ed9b9441
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-08-09 13:03:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,8 @@ happyDog = loadImage("dogImg1.png");
 function setup() {
   
   dog = createSprite(150,200,15,20);
-  dog.addImage(dogImage); 
+  dog.addImage(dogImage);
+  dog.scale = 0.5;
 
   database = firebase.database();
   foodStock = database.ref("food");
