283bf22abcd2cda6a683b9db4a731fb989d80936
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-08-10 20:56:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,6 +31,7 @@ function draw() {
   background(46,139,87);
   
   text("Press the Up Arrow Key to Feed Drago !",250,350);
+  text("Food Remaining + foodS",150,200);
   
   if(keyWentDown(UP_ARROW)){
   dog.addImage(happyDog);
